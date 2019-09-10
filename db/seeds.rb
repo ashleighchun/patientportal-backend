9bf5e859303aac1bba271158c55f9d653f2340b7
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-09-09 20:59:19 -0700

    add column to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 doctor = Doctor.create(name: "Dr. Joe", specialty: "acupuncture", city: "Brooklyn" )
 
 
-appointment = Appointment.create(doctor_id: 1, date: Date.today, reason: "shoulder pain", perscription: "none", follow_up_needed: true)
+appointment = Appointment.create(doctor_id: 1, date: Date.today, reason: "shoulder pain", copay: "20" perscription: "none", follow_up_needed: true)
 
 
 appointment_2 = Appointment.create(doctor_id: 1, date: Date.today, reason: "stomach aches", perscription: "herbs", follow_up_needed: false)
