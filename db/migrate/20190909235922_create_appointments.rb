7c5d4ca28c9c38393d48c02af3f5667b55576bfa
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-09-09 17:00:07 -0700

    generate appointment resources

---
+++ db/migrate/20190909235922_create_appointments.rb
@@ -0,0 +1,12 @@
+class CreateAppointments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :appointments do |t|
+      t.integer :doctor_id
+      t.datetime :date
+      t.string :reason
+      t.string :perscription
+
+      t.timestamps
+    end
+  end
+end
